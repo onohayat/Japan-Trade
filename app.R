da1d556d42d49d67dd46928778089a72934e1ca2
onohayat <55716664+onohayat@users.noreply.github.com>
2019-10-10 22:33:20 -0500

    Add files via upload

--- app.R
+++ app.R
@@ -1,30 +1,78 @@
----
-title: "Mid230"
-author: "Hayato Ono"
-date: "10/10/2019"
-output: html_document
----
 
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
-```
 
-## R Markdown
+ui <- fluidPage(  
+  # Create a title for app
+  column(3, titlePanel("Japanese Trade")),
+  column(12, 
+         # Create page for export map with slider input
+         tabsetPanel( tabPanel("Export Map",
+                               sliderInput(inputId = "Years", h3("Choose the Year"), 
+                                           min = 1962, max = 2017, 
+                                           value=1962, sep="", ticks = FALSE),
+                               leafletOutput("Map",height="600px")),
+                      # Create page for import map with slider input
+                      tabPanel("Import Map",
+                               sliderInput(inputId = "Years2", h3("Choose the Year"), 
+                                           min = 1962, max = 2017, 
+                                           value=1962, sep="", ticks = FALSE),
+                               leafletOutput("Map2", height="600px")),
+                      # Create page for graphing export and import values for user's preferred country
+                      tabPanel("Individual Country",
+                               selectizeInput("country", label = "Choose a Country", 
+                                              choices = AllCountriesAllYears$Destination
+                                              [AllCountriesAllYears$export_val!="na"],
+                                              options=list(create = FALSE)),
+                               plotlyOutput("Plot", height="600px")),   
+                      # Create page for comparing export and import values for different regions
+                      tabPanel("Region",
+                               fluidRow(column(width=12, 
+                                               plotlyOutput("Plot2", height="600px")))) ))) 
 
-This is an R Markdown document. Markdown is a simple formatting syntax for authoring HTML, PDF, and MS Word documents. For more details on using R Markdown see <http://rmarkdown.rstudio.com>.
+server <- function(input, output){
+  # Create map for export
+  qpal <- colorNumeric("YlOrBr", data1.2$log2, reverse = F)
+  output$Map <- renderLeaflet( {
+    leaflet(WorldCountry) %>% 
+      addTiles() %>% 
+      addPolygons(fillColor = 
+                    ~qpal(data1.2$log2[data1.2$Years==input$Years]), 
+                  weight=2, color="black", fillOpacity=0.7, 
+                  label=~paste(data1.2$Destination[data1.2$Years==input$Years],
+                               data1.2$export_val[data1.2$Years==input$Years])) %>%
+      setView(lng = 0, lat = 0, zoom = 2)} )
+  # Create map for import
+  qpal <- colorNumeric("YlOrBr", data1.2$log, reverse = F)
+  output$Map2 <- renderLeaflet( {
+    leaflet(WorldCountry) %>% 
+      addTiles() %>% 
+      addPolygons(fillColor = 
+                    ~qpal(data1.2$log[data1.2$Years==input$Years2]), 
+                  weight=2, color="black", fillOpacity=0.7, 
+                  label=~paste(data1.2$Destination[data1.2$Years==input$Years2]
+                               ,data1.2$import_val[data1.2$Years==input$Years2])) %>%
+      setView(lng = 0, lat = 0, zoom = 2)} )
+  # Name axes for line/bar graphs
+  x <- list(title="Year")
+  y <- list(title="Value")
+  x2 <- list(title="")
+  y2 <- list(title="Value")
+  # Create line graph for individual countries
+  output$Plot <- renderPlotly( {
+    plot_ly(data1.2,x=~Years[data1.2$Destination==input$country]) %>%
+      add_lines(y=~export_val[data1.2$Destination==input$country], 
+                name = "Export" ) %>%
+      add_lines(y=~import_val[data1.2$Destination==input$country], 
+                name = "Import") %>%
+      layout( title = ~paste("Trade between Japan and", input$country),
+              xaxis = x, yaxis = y) }) 
+  # Create bar graph for comparing regions
+  output$Plot2 <- renderPlotly( {
+    plot_ly(data2, x=~fct_reorder(Region,Export)) %>%
+      layout(xaxis = list(title=x2), 
+             yaxis= list(title=y2,inputautorange=TRUE)) %>%
+      add_bars(y=~Export/sum.Export., frame=~Years, name ="Export") %>%
+      add_bars(y=~Import/sum.Import., frame=~Years, name= "Import") })
+}
 
-When you click the **Knit** button a document will be generated that includes both content as well as the output of any embedded R code chunks within the document. You can embed an R code chunk like this:
 
-```{r cars}
-summary(cars)
-```
-
-## Including Plots
-
-You can also embed plots, for example:
-
-```{r pressure, echo=FALSE}
-plot(pressure)
-```
-
-Note that the `echo = FALSE` parameter was added to the code chunk to prevent printing of the R code that generated the plot.
+shinyApp(ui, server)
